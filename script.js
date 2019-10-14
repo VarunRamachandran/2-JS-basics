db9d7d6195382330eca5322d2ec8961ed6c1bc39
Varun Ramachandran <varun.r888@gmail.com>
2019-10-14 11:38:56 -0400

    9. Coercion mutation , prompt

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
+// var firstName = "Varun";
+// console.log(firstName);
+
+// var lastName = "Ramachandran";
+// var age = 29;
+// var fullAge = true;
+// console.log(fullAge);
+
+// var job;
+// console.log(job);
+
+// job = "teacher";
+// console.log(job);
+// //this the comment
+// var $3years = 3;
+// console.log($3years);
+
 var firstName = "Varun";
-console.log(firstName);
-
-var lastName = "Ramachandran";
 var age = 29;
-var fullAge = true;
-console.log(fullAge);
+console.log(firstName + " " + age);
 
-var job;
-console.log(job);
-
-job = "teacher";
-console.log(job);
-//this the comment
-var $3years = 3;
-console.log($3years);
+var job, isMarried;
+job = "driver";
+isMarried = false;
+alert(firstName + " " + "is a" + age + "year old" + job + ".Is he married?" + isMarried);
+var lastName = prompt("what is your last name");
+console.log(firstName + " " + lastName);
