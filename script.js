fa5521417d68c4d24a670f62f2256c32e965b40e
Varun Ramachandran <varun.r888@gmail.com>
2019-10-13 22:13:08 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var firstName = "Varun";
+console.log(firstName);
+
+var lastName = "Ramachandran";
+var age = 29;
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = "teacher";
+console.log(job);
+//this the comment
+var $3years = 3;
+console.log($3years);
