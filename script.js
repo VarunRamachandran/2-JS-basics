46c4d0274811e9222c9b240ec4dc6ca4b7004055
Varun Ramachandran <varun.r888@gmail.com>
2019-10-20 08:34:57 -0400

    15 if/else AND OR NOT logic

--- script.js
+++ script.js
@@ -1,33 +1,12 @@
-var firstName = "jhon";
-var civilStatus = "single";
+var firstName = "Jhon";
+var age = 20;
 
-if (civilStatus === "married") {
-  console.log(firstName + " " + "is married");
+if (age < 13) {
+  console.log(firstName + " " + "is a boy");
+} else if (age >= 13 && age < 20) {
+  console.log(firstName + " " + "is a teenager");
+} else if (age >= 20 && age < 30) {
+  console.log(firstName + " " + "is a young man");
 } else {
-  console.log(firstName + " will hopefully marry soon:}");
-}
-
-var isMarried = true;
-if (isMarried === true) {
-  console.log(firstName + " " + "is married");
-} else {
-  console.log(firstName + " will hopefully marry soon:}");
-}
-
-let markMass = 40;
-let markHeight = 1.7;
-let jhonMass = 30;
-let johnHeight = 1.8;
-
-let markBmi = markMass / (markHeight * 2);
-let jhonBmi = jhonMass / (johnHeight * 2);
-console.log(markBmi, jhonBmi);
-
-var greaterBmi = markBmi > jhonBmi;
-console.log("Is Mark's age greater than jhon " + " " + greaterBmi);
-
-if (markBmi > jhonBmi) {
-  console.log("Mark's Bmi is greater");
-} else {
-  console.log("Jhon's Bmi is greater");
+  console.log(firstName + " " + "is a man");
 }
