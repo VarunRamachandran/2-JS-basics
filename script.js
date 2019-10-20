a441bfa35c4756cf5d82e1e130254bdd9f29b6df
Varun Ramachandran <varun.r888@gmail.com>
2019-10-20 17:18:34 -0400

    16. Ternary and Switch Statement

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 var firstName = "Jhon";
-var age = 20;
+var age = 24;
+//terneary operator
+age >= 18 ? console.log(firstName + "  drink's beer.") : console.log(firstName + " drink's juice.");
 
-if (age < 13) {
-  console.log(firstName + " " + "is a boy");
-} else if (age >= 13 && age < 20) {
-  console.log(firstName + " " + "is a teenager");
-} else if (age >= 20 && age < 30) {
-  console.log(firstName + " " + "is a young man");
-} else {
-  console.log(firstName + " " + "is a man");
+var drink = age >= 18 ? "beer" : "juice";
+
+console.log(drink);
+
+// Switch Statement
+
+var job = "teacher";
+
+switch (job) {
+  case "teacher":
+    console.log(firstName + " Teaches kids how to code");
+    break;
+  case "driver":
+    console.log(firstName + "drives uber in lisbon");
+    break;
+  case "designer":
+    console.log(firstName + " Designs beautiful websites");
+    break;
+  default:
+    console.log(firstName + " does something else");
+}
+
+var age = 40;
+
+switch (true) {
+  case age < 13:
+    console.log(firstName + " is a boy");
+    break;
+  case age >= 13 && age < 20:
+    console.log(firstName + " is a teenager");
+    break;
+  case age > 20 && age < 30:
+    console.log(firstName + " is a young man");
+    break;
+  default:
+    console.log(firstName + " is a  man");
 }
