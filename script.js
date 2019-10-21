fb6838846633dfe98c5633e7fffd1e0d70635ad3
Varun Ramachandran <varun.r888@gmail.com>
2019-10-20 21:10:03 -0400

    17. === and == difference

--- script.js
+++ script.js
@@ -1,42 +1,12 @@
-var firstName = "Jhon";
-var age = 24;
-//terneary operator
-age >= 18 ? console.log(firstName + "  drink's beer.") : console.log(firstName + " drink's juice.");
+var height;
+height = 23;
 
-var drink = age >= 18 ? "beer" : "juice";
-
-console.log(drink);
-
-// Switch Statement
-
-var job = "teacher";
-
-switch (job) {
-  case "teacher":
-    console.log(firstName + " Teaches kids how to code");
-    break;
-  case "driver":
-    console.log(firstName + "drives uber in lisbon");
-    break;
-  case "designer":
-    console.log(firstName + " Designs beautiful websites");
-    break;
-  default:
-    console.log(firstName + " does something else");
+if (height || height === 0) {
+  console.log("Variable is defined");
+} else {
+  console.log("Variable has not be defined");
 }
 
-var age = 40;
-
-switch (true) {
-  case age < 13:
-    console.log(firstName + " is a boy");
-    break;
-  case age >= 13 && age < 20:
-    console.log(firstName + " is a teenager");
-    break;
-  case age > 20 && age < 30:
-    console.log(firstName + " is a young man");
-    break;
-  default:
-    console.log(firstName + " is a  man");
+if (height == "23") {
+  console.log("Variable is defined");
 }
