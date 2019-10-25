13c96720862d88aee880c867ccb1a146f2f551df
Varun Ramachandran <varun.r888@gmail.com>
2019-10-24 21:05:10 -0400

    Coding Challange 18 , 19

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-var height;
-height = 23;
+let teamJhonAvg = (89 + 120 + 103) / 3;
+let teamMikeAvg = (116 + 94 + 123) / 3;
 
-if (height || height === 0) {
-  console.log("Variable is defined");
+console.log(teamJhonAvg);
+console.log(teamMikeAvg);
+
+if (teamJhonAvg > teamMikeAvg) {
+  console.log(" Team Jhon is the winner with score of  " + teamJhonAvg);
+} else if (teamMikeAvg > teamJhonAvg) {
+  console.log(" Team Mike is the winner with score of  " + teamMikeAvg);
 } else {
-  console.log("Variable has not be defined");
+  console.log("Both teams are tied");
 }
 
-if (height == "23") {
-  console.log("Variable is defined");
+let teamMaryAvg = (97 + 134 + 105) / 3;
+console.log(teamMaryAvg);
+
+if (teamJhonAvg > teamMikeAvg && teamJhonAvg > teamMaryAvg) {
+  console.log(" Team Jhon is the winner with score of  " + teamJhonAvg);
+} else if (teamMikeAvg > teamJhonAvg && teamMikeAvg > teamMaryAvg) {
+  console.log(" Team Mike is the winner with score of  " + teamMikeAvg);
+} else if (teamMaryAvg > teamJhonAvg && teamMaryAvg > teamMikeAvg) {
+  console.log(" Team Mary is the winner with score of  " + teamMaryAvg);
+} else {
+  console.log("Its a draw");
 }
