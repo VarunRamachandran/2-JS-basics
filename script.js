33e2dfeaa4faca039e792def9d035c22972556b3
Varun Ramachandran <varun.r888@gmail.com>
2019-10-24 21:17:07 -0400

    21 function basics

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-let teamJhonAvg = (89 + 120 + 103) / 3;
-let teamMikeAvg = (116 + 94 + 123) / 3;
-
-console.log(teamJhonAvg);
-console.log(teamMikeAvg);
-
-if (teamJhonAvg > teamMikeAvg) {
-  console.log(" Team Jhon is the winner with score of  " + teamJhonAvg);
-} else if (teamMikeAvg > teamJhonAvg) {
-  console.log(" Team Mike is the winner with score of  " + teamMikeAvg);
-} else {
-  console.log("Both teams are tied");
+function claculateAge(birthYear) {
+  return 2018 - birthYear;
 }
 
-let teamMaryAvg = (97 + 134 + 105) / 3;
-console.log(teamMaryAvg);
+var ageJhon = claculateAge(1990);
+var ageJane = claculateAge(1940);
+var ageMike = claculateAge(1930);
+console.log(ageJhon, ageJane, ageMike);
 
-if (teamJhonAvg > teamMikeAvg && teamJhonAvg > teamMaryAvg) {
-  console.log(" Team Jhon is the winner with score of  " + teamJhonAvg);
-} else if (teamMikeAvg > teamJhonAvg && teamMikeAvg > teamMaryAvg) {
-  console.log(" Team Mike is the winner with score of  " + teamMikeAvg);
-} else if (teamMaryAvg > teamJhonAvg && teamMaryAvg > teamMikeAvg) {
-  console.log(" Team Mary is the winner with score of  " + teamMaryAvg);
-} else {
-  console.log("Its a draw");
+function yearsUntilRetirement(year, firstName) {
+  var age = claculateAge(year);
+  var retirement = 65 - age;
+  if (retirement > 0) {
+    console.log(firstName + " retires in" + "  " + retirement + "years");
+  } else {
+    console.log(firstName + " is already retired");
+  }
 }
+
+yearsUntilRetirement(1990, "Jhon");
+yearsUntilRetirement(1930, "MIke");
+yearsUntilRetirement(1950, "Jane");
