445105e174345bef1a36f06fe502605d8290e6f6
Varun Ramachandran <varun.r888@gmail.com>
2019-10-27 13:32:18 -0400

    22 Arrays

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 //Function Decleration
 
 //Function expresion
-var whatDoYouDo = function(job, firstName) {
-  switch (job) {
-    case "teacher":
-      return firstName + " teaches kids how to code";
-    case "driver":
-      return firstName + " divers a cab in lisbon";
-    case "designer":
-      return firstName + " designs beautiful websites";
-    default:
-      return firstName + " does something else";
-  }
-};
+var names = ["Jhon", "Jane", "Mark"];
+var years = new Array(1990, 1969, 1948);
+console.log(names);
+console.log(names.length);
 
-console.log(whatDoYouDo("teacher", "Jhon"));
-console.log(whatDoYouDo("designer", "Jane"));
-console.log(whatDoYouDo("retired", "Mark"));
+names[1] = "Ben";
+names[5] = "Mary";
+names[names.length] = "Amy";
+console.log(names);
+
+var jhon = ["Jhon", "Smith", 1990, "designer", false];
+
+jhon.push("blue");
+jhon.unshift("Mr.");
+jhon.pop();
+jhon.pop();
+jhon.shift();
+var isDesigner = jhon.indexOf("designer") === -1 ? "Jhon is NOT a designer" : "Jhon IS a designer";
+console.log(isDesigner);
