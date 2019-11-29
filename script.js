333161978b45da7dad7bb1aa294f28502a88c8dc
Varun Ramachandran <varun.r888@gmail.com>
2019-11-28 22:48:43 -0500

    Coding challenge 30. 31 - better version

--- script.js
+++ script.js
@@ -1,73 +1,24 @@
-let jhon = [124, 48, 268, 180, 42];
-let mark = [77, 375, 110, 45];
-
-let jhontip = [];
-let jhonfinalAmt = [];
-
-let marktip = [];
-let markfinalAmt = [];
-
-let markAvg = 0;
-let jhonAvg = 0;
-
-let jhonAvgFinal = [];
-let markAvgFinal = [];
-
-function jhonCalltip(amt) {
-  if (amt > 50) {
-    jhontip.push(amt * 0.2);
-    jhonfinalAmt.push(amt * 0.2 + amt);
-  } else if (amt > 50 && amt < 200) {
-    jhontip.push(amt * 0.15);
-    jhonfinalAmt.push(amt * 0.15 + amt);
-  } else if (amt > 200) {
-    jhontip.push(amt * 0.1);
-    jhonfinalAmt.push(amt * 0.1 + amt);
+let jhon = {
+  fullName: "Jhon Smith",
+  bills: [124, 48, 268, 180, 42],
+  calTips: function() {
+    this.jhontip = [];
+    this.jhonfinalAmt = [];
+    for (var i = 0; i < this.bills.length; i++) {
+      var bill = this.bills[i];
+      if (bill < 50) {
+        this.jhontip.push(bill * 0.2);
+        this.jhonfinalAmt.push(bill * 0.2 + bill);
+      } else if (bill > 50 && bill < 200) {
+        this.jhontip.push(bill * 0.15);
+        this.jhonfinalAmt.push(bill * 0.15 + bill);
+      } else if (bill > 200) {
+        this.jhontip.push(bill * 0.1);
+        this.jhonfinalAmt.push(bill * 0.1 + bill);
+      }
+    }
   }
-}
+};
 
-function markCaltip(amt) {
-  if (amt > 50) {
-    marktip.push(amt * 0.2);
-    markfinalAmt.push(amt * 0.2 + amt);
-  } else if (amt > 50 && amt < 200) {
-    marktip.push(amt * 0.15);
-    markfinalAmt.push(amt * 0.15 + amt);
-  } else if (amt > 200) {
-    marktip.push(amt * 0.1);
-    markfinalAmt.push(amt * 0.1 + amt);
-  }
-}
-
-for (var i = 0; i < jhon.length; i++) {
-  jhonCalltip(jhon[i]);
-}
-for (var i = 0; i < mark.length; i++) {
-  markCaltip(mark[i]);
-}
-
-function JhonAvgTip() {
-  for (var i = 0; i < jhontip.length; i++) {
-    jhonAvg += jhontip[i];
-  }
-  jhonAvgFinal = jhonAvg / jhontip.length;
-}
-
-function markAvgTip() {
-  for (var i = 0; i < marktip.length; i++) {
-    markAvg += marktip[i];
-  }
-  markAvgFinal = markAvg / marktip.length;
-}
-
-JhonAvgTip();
-markAvgTip();
-
-console.log(jhontip);
-console.log(jhonfinalAmt);
-
-console.log(marktip);
-console.log(markfinalAmt);
-
-console.log(jhonAvgFinal);
-console.log(markAvgFinal);
+jhon.calTips();
+console.log(jhon);
